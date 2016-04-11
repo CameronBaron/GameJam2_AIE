9b5cf6e4ae478d8b46da0f68c99589761e15b40c
Cameron Baron <cameron.baron@ad.aie.edu>
2016-04-11 16:19:10 +1000

    Added Cam test scene, player throwing, ball movement and prefab.

---
+++ New Unity Project/Assets/Cam Testing/Ball.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof (Rigidbody))]
+
+public class Ball : MonoBehaviour
+{
+	Rigidbody rb;
+	PhysicMaterial pmat;
+
+	public float lifeTime = 20;
+	private float lifetTimeDefault = 20;
+	private float lifeCounter = 0;
+
+	private float bounces = 0;
+
+	//Powerup Vars
+	private int maxBounces = 10000;             // For Rikochet ball
+	private int maxBouncesDefault = 1;
+	private float scale = 1;					// For Giant Ball
+	private GameObject target = null;               // For Homing ball
+
+	// Physics Mat
+	private float bounciness = 1;	
+
+	// Use this for initialization
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		pmat = GetComponent<PhysicMaterial>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (rb.velocity != Vector3.zero)
+			lifeCounter += Time.deltaTime;
+
+		if (lifeCounter > lifeTime || bounces > maxBounces)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	// On Collision Enter
+	void OnCollisionEnter(Collision col)
+	{
+		foreach (ContactPoint contact in col.contacts)
+		{
+			//Debug.DrawRay(contact.point, contact.normal, Color.white);
+		}
+
+		if (col.gameObject.tag == "Player")
+		{
+			// remove health
+		}
+		else
+		{
+			bounces++;
+		}
+
+		if (col.relativeVelocity.magnitude > 2)
+		{
+			//play audio
+		}
+	}
+}
---
+++ New Unity Project/Assets/Cam Testing/Shooting.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using InControl;
+
+public class Shooting : MonoBehaviour
+{
+	/*
+	Invincibility
+	Ricochet ball
+	Homo ball
+	Speed
+	ELEPHANTITIS Balls!
+
+		Move speed
+		Health pickup
+		
+	*/
+	InputDevice input { get; set; }
+	public GameObject ball;
+	public GameObject rightHand;
+
+	public int ammo = 1;
+	public int ballSpeed = 100;
+
+	public float chargeUpTimer = 4;
+	private float chargeUpCounter = 0;
+	
+	bool holdingRight = false;
+
+	private GameObject rightBall;
+
+	private Transform playerTransform;
+
+	// Use this for initialization
+	void Start ()
+	{
+		playerTransform = GetComponentInParent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		input = InputManager.ActiveDevice;
+		if (!holdingRight) // If not holding in one hand, check to see if we should be
+			HoldBalls();
+
+		Throw();
+		Block();
+		DropBall();
+	}
+
+	void HoldBalls()
+	{
+		if (ammo > 0 && !holdingRight)
+		{
+			rightBall = Instantiate(ball, rightHand.transform.position, Quaternion.identity) as GameObject;
+			rightBall.GetComponent<Rigidbody>().useGravity = false;
+			rightBall.tag = "Ball";
+			holdingRight = true;
+			ammo--;
+		}
+	}
+
+	void Throw()
+	{
+		if (holdingRight && input.RightTrigger.IsPressed)
+		{
+			chargeUpCounter += Time.deltaTime;
+		}
+		// On right/left trigger press
+		// If ball in hand
+		// Throw (apply force) in players forward direction
+		if (holdingRight && input.RightTrigger.WasReleased)
+		{
+			rightBall.GetComponent<Rigidbody>().useGravity = true;
+			rightBall.GetComponent<Rigidbody>().AddForce(playerTransform.forward * ballSpeed * chargeUpCounter * 5, ForceMode.Impulse);
+			holdingRight = false;
+			chargeUpCounter = 0;
+			rightBall = null;
+		}
+	}
+
+	void Block()
+	{
+	 // If left/right bumper pressed
+		if (input.RightBumper.WasPressed)
+		{
+		//  Move hand to block/catch position
+			// If ball collides with front of player
+			if (ammo == 0)
+			{
+				// Catch
+				ammo++;
+			}
+			else
+			{
+				// Block
+			}
+			
+		}
+	}
+
+	void DropBall()
+	{
+		if (input.Action2.IsPressed && holdingRight)
+		{
+			rightBall.GetComponent<Rigidbody>().useGravity = true;
+			holdingRight = false;
+			rightBall = null;
+		}
+	}
+}
