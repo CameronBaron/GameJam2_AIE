8ae019928fba25637cbe30607ee928ca0fd41fdb
Simon Rodhouse <sr32609@gmail.com>
2016-04-11 16:41:38 +1000

    create powerup objects/scripts

---
+++ New Unity Project/Assets/Scripts/Powerups/PowerUpScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpScript : MonoBehaviour {
+
+	/* poweruptypes
+		
+		ricochet = 5?
+		invincibility
+		guided ball
+		super-ball
+
+	*/
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
