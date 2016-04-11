b81e19169ab357f9ae5a8cc6ad8b92b8a26673f2
Cameron Baron <cameron.baron@ad.aie.edu>
2016-04-11 11:04:20 +1000

    Added Cam Testing scene

---
+++ New Unity Project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+	InControl.InputControl input;
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
